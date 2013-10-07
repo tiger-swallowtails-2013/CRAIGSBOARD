6a922d77d5ac04441660b3f9fa6d9f87bcea73c3
Apprentice <apprentice@dbc-21.local>
2013-10-07 12:28:10 -0700

    fisrt commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Craigsboard::Application.config.secret_key_base = 'c245c9b1d9536fb47cc15589f76d72d81666fd4220c5553936ff25bc644cdbe0d55a4764e12d5f147e73f00d4c34714876779eda9bb0d6d9215b5f6fd2b71753'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Craigsboard::Application.config.session_store :cookie_store, key: '_craigsboard_session'
