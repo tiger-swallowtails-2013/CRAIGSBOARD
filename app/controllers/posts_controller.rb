23c310eb2ac9ab6a6c818a18f46fa0721059f378
Apprentice <apprentice@dbc-19.local>
2013-10-08 11:28:07 -0700

    connect new posts to topic_id

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,8 +1,25 @@
 class PostsController < ApplicationController
 
   def index
-    @posts = Post.all
+    @topic = Topic.find(params[:topic_id])
+    @posts = @topic.posts
   end
+
+
+  def create
+
+    @post = Post.create(params[:post])
+    redirect_to post_path(@post)
+  end
+
+  def show
+    @post = Post.find(params[:id])
+  end
+
+  def new
+
+  end
+
 
 
 
